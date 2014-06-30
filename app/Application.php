70ad39281e3c15f08e5cebee120955c9ac7c9e4d
Mike Gibson <mike@mgdigital.co.uk>
2014-06-30 19:05:56 +0100

    Added some comments

--- app/Application.php
+++ app/Application.php
@@ -7,6 +7,10 @@ use Layer\Pages\PagesPlugin;
 
 class Application extends \Layer\Application {
 
+	/**
+	 * Initialize the app
+	 * Any custom services and configuration for the app should be initialized here
+	 */
 	protected function initialize() {
 
 		$app = $this;
@@ -17,14 +21,22 @@ class Application extends \Layer\Application {
 		$app['name'] = 'Layer Skeleton App';
 
 		/**
-		 * Register plugins
+		 * Register plugins and service providers
 		 */
 		$app
 			->register(new PagesPlugin())
 			->register(new BlogPlugin())
 		;
 
+		/**
+		 * Register CSS assets
+		 */
 		$app['assets.register_scss']('app', ['scss/app.scss']);
+
+		/**
+		 * Register javascript assets
+		 */
+		// $app['assets.register_scss']('app', ['scss/app.scss']);
 
 	}
 

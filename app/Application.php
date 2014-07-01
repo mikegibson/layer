750b959517cb537bbdcb232a5ef410c75c3af16a
Mike Gibson <mike@mgdigital.co.uk>
2014-07-01 13:59:59 +0100

    Added "about" action

--- app/Application.php
+++ app/Application.php
@@ -2,8 +2,10 @@
 
 namespace App;
 
+use Sentient\Action\SimpleAction;
 use Sentient\Application as Sentient;
 use Sentient\Blog\BlogPlugin;
+use Sentient\Node\ControllerNode;
 use Sentient\Pages\PagesPlugin;
 
 class Application extends Sentient {
@@ -45,6 +47,21 @@ class Application extends Sentient {
 		 * Register javascript assets
 		 */
 		// $app['assets.register_js']('app', ['js/app.js']);
+
+		/**
+		 * Create an "about" action for displaying the About page
+		 */
+		$app['about_action'] = $app->share(function() {
+			return new SimpleAction('about', 'About', 'view/about');
+		});
+
+		/**
+		 * Add the about action to the home node
+		 */
+		$app['home_node'] = $app->share($app->extend('home_node', function(ControllerNode $homeNode) use($app) {
+			$homeNode->wrapChildNode(new ControllerNode($app['about_action']));
+			return $homeNode;
+		}));
 
 	}
 
